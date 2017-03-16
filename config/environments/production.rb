cc3c9091a68401a893ebab457b2f09b44d82a9fa
RALIST <danilov@ralist.ru>
2017-03-16 18:11:09 +0400

    12 factors remove

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,6 +80,17 @@ Rails.application.configure do
     logger.formatter = config.log_formatter
     config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
+  unless Rails.env.test?
+    config.paperclip_defaults = {
+      storage: :s3,
+        s3_region: ENV['AWS_REGION'],
+        s3_host_name: "s3-us-west-2.amazonaws.com",
+        s3_credentials: {
+          bucket: ENV['AWS_BUCKET'],
+          access_key_id: ENV['AWS_ACCESS_KEY'],
+          secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+        }
+    }
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
